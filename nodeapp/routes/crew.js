0de42a0321e230a132524698f8ffbef17e01791f
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-12-24 14:03:15 +0000

    add views for all tables

--- nodeapp/routes/crew.js
+++ nodeapp/routes/crew.js
@@ -4,10 +4,10 @@ var db = require('../database');
 // another routes also appear here
 // this script to fetch data from MySQL databse table
 router.get('/', function (req, res) {
-  var sql = 'SELECT * FROM acteur';
+  var sql = 'SELECT * FROM crew';
   db.query(sql, function (err, data, fields) {
     if (err) throw err;
-    res.render('user-list', { title: 'Acteur List', listData: data });
+    res.render('crew-list', { title: 'Crew List', listData: data });
   });
 });
 module.exports = router;
--- nodeapp/routes/filmcrew.js
+++ nodeapp/routes/filmcrew.js
@@ -4,10 +4,10 @@ var db = require('../database');
 // another routes also appear here
 // this script to fetch data from MySQL databse table
 router.get('/', function (req, res) {
-  var sql = 'SELECT * FROM acteur';
+  var sql = 'SELECT * FROM filmcrew';
   db.query(sql, function (err, data, fields) {
     if (err) throw err;
-    res.render('user-list', { title: 'Acteur List', listData: data });
+    res.render('filmcrew-list', { title: 'Film Crew List', listData: data });
   });
 });
 module.exports = router;
